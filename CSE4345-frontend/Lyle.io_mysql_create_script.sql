b9537562680d93b6068cf4e01a1ece03389b6cda
Noah Schweser <nschweser@smu.edu>
2018-11-27 23:14:37 -0600

    Added CORS to routes and cleaned up SQL Build script

--- CSE4345-frontend/Lyle.io_mysql_create_script.sql
+++ CSE4345-frontend/Lyle.io_mysql_create_script.sql
@@ -16,9 +16,9 @@ CREATE TABLE Users (
 	PRIMARY KEY (userID)
 );
 Insert into Users (userName, pass, email) values ("admin", "password", "admin@lyle.io");
-Insert into users (userName, pass, track, email, firstName, lastName, gradYear, bio) values ("Test","testpass","CSE", "test@test.net", "Test", "User", 2018, "I am a test user and literally do not exist");
-Insert into users (userName, pass, track, email, firstName, lastName) values ("JSmith","JohnPass","Cyber Security", "JSmith@cyber.com", "John", "Smith", 2020, "This is my biography and it has words in it");
-Insert into users (userName, pass, track, email, firstName, lastName) values ("NSchweser","pw","Game Development", "Schweser@smu.edu", "Noah", "Schweser", 2019, "Smash bros lol");
+#Insert into users (userName, pass, track, email, firstName, lastName, gradYear, bio) values ("Test","testpass","CSE", "test@test.net", "Test", "User", 2018, "I am a test user and literally do not exist");
+#Insert into users (userName, pass, track, email, firstName, lastName) values ("JSmith","JohnPass","Cyber Security", "JSmith@cyber.com", "John", "Smith", 2020, "This is my biography and it has words in it");
+#Insert into users (userName, pass, track, email, firstName, lastName) values ("NSchweser","pw","Game Development", "Schweser@smu.edu", "Noah", "Schweser", 2019, "Smash bros lol");
 
 drop table if exists Forums;
 CREATE TABLE Forums (
@@ -26,11 +26,11 @@ CREATE TABLE Forums (
 	forumName varchar(20) NOT NULL DEFAULT 'newForum',
 	PRIMARY KEY (forumID)
 );
-Insert into Forums (forumName) values ("CSE (General)");
-Insert into Forums (forumName) values ("Cyber Security");
-Insert into Forums (forumName) values ("Game Development");
-Insert into Forums (forumName) values ("Pre Med");
-Insert into Forums (forumName) values ("Data Intensive");
+#Insert into Forums (forumName) values ("CSE (General)");
+#Insert into Forums (forumName) values ("Cyber Security");
+#Insert into Forums (forumName) values ("Game Development");
+#Insert into Forums (forumName) values ("Pre Med");
+#Insert into Forums (forumName) values ("Data Intensive");
 
 drop table if exists Posts;
 CREATE TABLE Posts (
@@ -41,16 +41,16 @@ CREATE TABLE Posts (
 	forumID int NOT NULL,
 	postdate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (postID),
-	FOREIGN KEY(forumID) REFERENCES Forums(forumID)
+	FOREIGN KEY(forumID) REFERENCES Forums(forumID) ON DELETE CASCADE
 );
-Insert into Posts (userName, title, body, forumID, postdate) values ("Test", "Need help choosing a track","I dunno what track I want to be on and need advice on how to choose",1, '2018-01-01 00:00:00');
-Insert into Posts (userName, title, body, forumID) values ("JSmith", "Encryption","I need to figure out how to encrypt this data for homework",2);
-Insert into Posts (userName, title, body, forumID) values ("NSchweser", "Best game engine?","Unity or Unreal? Please help me decide I am very poor",3);
-Insert into Posts (userName, title, body, forumID) values ("NSchweser", "Come to Cyber Security Club!","It's gonna be great, I really think you'll enjoy it!",2);
-Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Data Intensive Club!","It's gonna be great, I really think you'll enjoy it!",5, '2017-01-01 00:00:00');
-Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Pre Med Club!","It's gonna be great, I really think you'll enjoy it!",4, '2010-01-01 00:00:00');
-Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Pre Med Club!","It's gonna be great, I really think you'll enjoy it!",4, '2009-01-01 00:00:00');
-Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Pre Med Club!","It's gonna be great, I really think you'll enjoy it!",4, '2008-01-01 00:00:00');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("Test", "Need help choosing a track","I dunno what track I want to be on and need advice on how to choose",1, '2018-01-01 00:00:00');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("JSmith", "Encryption","I need to figure out how to encrypt this data for homework",2,'2018-11-13 12:00:00');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("NSchweser", "Best game engine?","Unity or Unreal? Please help me decide I am very poor",3,'2017-12-31 23:56:32');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("NSchweser", "Come to Cyber Security Club!","It's gonna be great, I really think you'll enjoy it!",2,'2018-08-30 15:43:06');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Data Intensive Club!","It's gonna be great, I really think you'll enjoy it!",5, '2017-01-01 00:00:00');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Pre Med Club!","It's gonna be great, I really think you'll enjoy it!",4, '2010-01-01 00:00:00');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Pre Med Club!","It's gonna be great, I really think you'll enjoy it!",4, '2009-01-01 00:00:00');
+#Insert into Posts (userName, title, body, forumID, postdate) values ("admin", "Come to Pre Med Club!","It's gonna be great, I really think you'll enjoy it!",4, '2008-01-01 00:00:00');
 
 drop table if exists Messages;
 CREATE TABLE Messages (
@@ -60,9 +60,9 @@ CREATE TABLE Messages (
 	body varchar(512) NOT NULL,
 	postdate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (msgID),
-	FOREIGN KEY(postID) REFERENCES Posts(postID)
+	FOREIGN KEY(postID) REFERENCES Posts(postID) ON DELETE CASCADE
 );
-Insert into Messages (userName, postID, body) values ("NSchweser", 1,"Try game development!");
-Insert into Messages (userName, postID, body) values ("JSmith", 4,"Sounds fun! What time is it?");
-Insert into Messages (userName, postID, body) values ("admin", 4,"It's at 6pm tomorrow in Caruth. Should be tons of fun!");
-Insert into Messages (userName, postID, body) values ("admin", 5,"It's at 6pm tomorrow in Caruth. Should be tons of fun!");
+#Insert into Messages (userName, postID, body) values ("NSchweser", 1,"Try game development!");
+#Insert into Messages (userName, postID, body) values ("JSmith", 4,"Sounds fun! What time is it?");
+#Insert into Messages (userName, postID, body) values ("admin", 4,"It's at 6pm tomorrow in Caruth. Should be tons of fun!");
+#Insert into Messages (userName, postID, body) values ("admin", 5,"It's at 6pm tomorrow in Caruth. Should be tons of fun!");
--- CSE4345-frontend/Lyle/src/routes.php
+++ CSE4345-frontend/Lyle/src/routes.php
@@ -1,6 +1,19 @@
 <?php
 use Slim\Http\Request;
 use Slim\Http\Response;
+header("Access-Control-Allow-Origin: *");
+
+$app->options('/{routes:.+}', function($request, $response, $args){
+	return $response;
+});
+	
+$app->add(function ($req, $res, $next) {
+	$response = $next($req, $res);
+	return $response
+		->withHeader('Access-Control-Allow-Origin', '*')
+		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 // Routes
 $app->post('/login', function ($request, $response) {
@@ -221,4 +234,11 @@ $app->group('/forums', function () use ($app) {
 			});
 		});
 	});
+});
+
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
 });
